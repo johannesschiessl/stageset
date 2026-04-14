e512f174e276e7b019eec15cf1bbcd2ca585af7a
Johannes <150372753+johannesschiessl@users.noreply.github.com>
2026-04-14 20:05:04 +0200

    Shadcn

--- src/client/routes/index.tsx
+++ src/client/routes/index.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/client/components/ui/Button";
+
 export function Index() {
   return (
     <div>
       <h1>Stageset</h1>
+      <Button>LIVE</Button>
     </div>
   );
 }
